6d3bd232c44a2e7808edf5f2fbcb1b7e46d7b347
Hugo Porcher <hugo.porcher@eset.com>
2019-01-10 16:14:51 -0500

    Add other architectures (x86_64, ARM, MIPS and PPC)

--- __init__.py
+++ __init__.py
@@ -8,17 +8,22 @@ def stop_sentinelle(jitter):
     return True
 
 def emulate(bv, addr):
-    choice = get_choice_input("Ready to emulate code?", "[Miasm] Emulation", ["Yes", "No"])
-    if choice is 2:
-        return
+    archs = { "x86":"x86_32", "x86_64":"x86_64", "armv7":"arml", "aarch64":"aarch64l", "mips32":"mips32l", "powerpc":"ppc32b"}
     stop_addr = get_address_input("Stop emulation at (address):", "[Miasm] Emulation")
-    jitter = Machine("x86_32").jitter("gcc")
+    try:
+        jitter = Machine(archs[bv.arch.name]).jitter("gcc")
+    except:
+        show_message_box("[Miasm] Emulation", "Jitter not available for this architecture (" + archs[bv.arch.name] + ")", MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.ErrorIcon)
+        return
     jitter.init_stack()
-    data = bv.read(bv.start, bv.end-bv.start)
-    jitter.vm.add_memory_page(bv.start, PAGE_READ | PAGE_WRITE, data)
+    for s in bv.segments:
+        data = bv.read(s.start, len(s))
+        jitter.vm.add_memory_page(s.start, PAGE_READ | PAGE_WRITE, data)
+    jitter.set_trace_log()
     jitter.add_breakpoint(stop_addr, stop_sentinelle)
     jitter.init_run(addr)
     jitter.continue_run()
-    bv.write(bv.start, jitter.vm.get_mem(bv.start, bv.end-bv.start))
+    for s in bv.segments:
+        bv.write(s.start, jitter.vm.get_mem(s.start, len(s)))
 
 PluginCommand.register_for_address("[Miasm] Emulate from this instruction", "Execute the code in a sandbox and apply the changes on the BNDB", emulate)
